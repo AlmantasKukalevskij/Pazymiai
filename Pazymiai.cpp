c9b6ffcaffbd73b393e917ef5d41c6abe0966137
AlmantasKukalevskij <92589309+AlmantasKukalevskij@users.noreply.github.com>
2022-03-05 20:55:58 +0200

    Sutvarkytas

--- Pazymiai.cpp
+++ Pazymiai.cpp
@@ -19,20 +19,22 @@ struct mokinys
 {
     string vardas = "", pavarde = "";
     vector<int> pazymiai;
-    //int pazymiuSk = 0;
+    int pazymiuSk = 0;
     int egzaminas;
     double rezult = 0;
+    double vidurkis = 0;
+    double mediana = 0;
 };
 
 void ivestis(mokinys& temp, bool kurimas);
-void isved(mokinys& temp, bool mediana, std::ofstream &fout);
-//ka fout daro? iraso info i txt?
+void isved(mokinys& temp, ofstream& fout);
 //void RandomNdIvedimas(mokinys& temp, int pazimys);
 bool PazTikrinimas(int t);
 int pazymioIvedimas();
 int IntTikrinimas();
 string atsakymoTikrinimas();
-void skaitymas(ifstream& fin, vector<string>& length, vector<mokinys> &mokiniai);
+void skaitymas(ifstream& fin, vector<string>& length, vector<mokinys>& mokiniai);
+
 
 
 //bool egzaminas;
@@ -59,22 +61,33 @@ int main()
     //mokinys* VisiMokiniai = new mokinys[mokiniuSk];
 
     vector<mokinys> VisiMokiniai;
+    std::ofstream fout("atsakymai.txt");
 
     cout << "Ar norite vesti pazymius ranka (taip), kitaip (ne) pazymiai bus nuskaityti is failo" << endl;
     failoNuskaitymas = atsakymoTikrinimas();
 
-    vector<string> length; 
-    vector<mokinys> mokiniai;
-    std::ofstream fout("rezultatai.txt");
 
-    if (suvedimas == false)
+    if (failoNuskaitymas == "ne")
     {
+        vector<string> length;
         std::ifstream fin("mokiniai.txt");
-            skaitymas(fin, length, mokiniai);
+        skaitymas(fin, length, VisiMokiniai);
     }
     else
     {
-        //sukeliam visus pries tai keltus klausimus i else, jeigu pasirenkama irasyti info ranka
+
+        cout << " Ar norite gauti pazymiu mediana(taip), kitaip bus skaiciuojamas vidurkis (ne) ";
+        Mediana = atsakymoTikrinimas();
+        if (Mediana == "taip")
+        {
+            cout << "Atsakymas bus skaiciuojamas su mediana" << endl;
+            mediana = true;
+        }
+        else
+        {
+            cout << "Atsakymas bus skaiciuojamas su vidurkiu" << endl;
+        }
+
         cout << "Ar mokiniu skaicius yra zinomas (taip/ne)?";
         MokiniuSkaicius = atsakymoTikrinimas();
         if (MokiniuSkaicius == "taip")
@@ -88,79 +101,56 @@ int main()
         }
 
 
-
-    }
-
-    cout << " Ar norite gauti pazymiu mediana(taip), kitaip bus skaiciuojamas vidurkis (ne) ";
-    Mediana = atsakymoTikrinimas();
-    if (Mediana == "taip")
-    {
-        cout << "Atsakymas bus skaiciuojamas su mediana" << endl;
-        mediana = true;
-    }
-    else
-    {
-        cout << "Atsakymas bus skaiciuojamas su vidurkiu" << endl;
-    }
-
-
-    cout << "Ar mokiniu skaicius yra zinomas (taip/ne)?";
-    MokiniuSkaicius = atsakymoTikrinimas();
-    if (MokiniuSkaicius == "taip")
-    {
-        cout << "Mokiniu skaicius zinomas" << endl;
-        mokiniuSkaicius = true;
-    }
-    else
-    {
-        cout << "Mokiniu skaicius nezinomas" << endl;
-    }
-
-    cout << "Ar norite jog namu darbu pazymiai butu sugeneruoti automatiskai (taip/ne)?";
-    generuotiNd = atsakymoTikrinimas();
-    if (generuotiNd == "taip")
-    {
-        cout << "Namu darbu pazymiai bus automatiskai sugeneruoti" << endl;
-        kurimas = true;
-    }
-    else
-    {
-        cout << "Namu darbu pazymiai nebus automatiskai generuojami" << endl;
-    }
-
-
-    if (mokiniuSkaicius)
-    {
-        cout << "Iveskite mokiniu skaiciu: ";
-        mokiniuSk = IntTikrinimas();
-        for (int i = 0; i < mokiniuSk; i++)
+        cout << "Ar norite jog namu darbu pazymiai butu sugeneruoti automatiskai (taip/ne)?";
+        generuotiNd = atsakymoTikrinimas();
+        if (generuotiNd == "taip")
         {
-            mokinys data;
-            ivestis(data, kurimas);
-            VisiMokiniai.push_back(data);
+            cout << "Namu darbu pazymiai bus automatiskai sugeneruoti" << endl;
+            kurimas = true;
         }
-    }
-    else
-    {
-        string mok;
-        while (true)
+        else
         {
-            cout << "Ar norite irasyti dar viena mokini? (taip/ne) ";  mok = atsakymoTikrinimas();
-            if (mok == "ne")
-                break;
-            mokinys data;
-            ivestis(data, kurimas);
-            VisiMokiniai.push_back(data);
+            cout << "Namu darbu pazymiai nebus automatiskai generuojami" << endl;
         }
 
+        if (mokiniuSkaicius)
+        {
+            cout << "Iveskite mokiniu skaiciu: ";
+            mokiniuSk = IntTikrinimas();
+            for (int i = 0; i < mokiniuSk; i++)
+            {
+                mokinys data;
+                ivestis(data, kurimas);
+                VisiMokiniai.push_back(data);
+            }
+        }
+        else
+        {
+            string mok;
+            while (true)
+            {
+                cout << "Ar norite irasyti dar viena mokini? (taip/ne) ";  mok = atsakymoTikrinimas();
+                if (mok == "ne")
+                    break;
+                mokinys data;
+                ivestis(data, kurimas);
+                VisiMokiniai.push_back(data);
+            }
+
+        }
+
+
     }
 
-    std::sort(mokiniai.begin(), mokiniai.end(), [](mokinys& a, mokinys& b) {return a.vardas < b.vardas; });;
 
-    if (mediana) cout << std::setw(20) << "PAVARDE" << std::setw(20) << "VARDAS" << std::setw(20) << "GALUTINE MEDIANA" << endl;
-    else cout << std::setw(20) << "PAVARDE" << std::setw(20) << "VARDAS" << std::setw(20) << "GALUTINIS VIDURKIS" << endl;
 
-    for (mokinys mokinys : VisiMokiniai) isved(mokinys, mediana);
+    std::sort(VisiMokiniai.begin(), VisiMokiniai.end(), [](mokinys& a, mokinys& b) {return a.vardas < b.vardas; });
+    fout << std::setw(20) << "VARDAS" << std::setw(20) << "PAVARDE" << std::setw(20) << "VIDURKIS" << std::setw(20) << "MEDIANA" << endl;
+    fout << "====================================================================================" << endl;
+
+    for (mokinys mokinys : VisiMokiniai) isved(mokinys, fout);
+
+
 }
 void ivestis(mokinys& temp, bool kurimas)
 {
@@ -178,7 +168,7 @@ void ivestis(mokinys& temp, bool kurimas)
     {
         for (int i = 0; i < temp.pazymiuSk; i++)
         {
-            temp.pazymiai[i] = rand() % 10 + 1;
+            temp.pazymiai.push_back(rand() % 10 + 1);
             cout << "Ivestas" << i + 1 << " pazimys bus: " << temp.pazymiai[i] << endl;
         }
         temp.egzaminas = rand() % 10 + 1;
@@ -188,7 +178,7 @@ void ivestis(mokinys& temp, bool kurimas)
     {
         for (int i = 0; i < temp.pazymiuSk; i++) {
             cout << "Iveskite " << i + 1 << " -a(-i) pazymi: ";
-            temp.pazymiai[i] = pazymioIvedimas();
+            temp.pazymiai.push_back(pazymioIvedimas());
         }
         cout << "Veskite egzamino ivertinima: ";
         temp.egzaminas = pazymioIvedimas();
@@ -198,26 +188,20 @@ void ivestis(mokinys& temp, bool kurimas)
 
 }
 
-void isved(mokinys& temp, bool mediana)
+void isved(mokinys& temp, ofstream& fout)
 {
-    cout << std::setw(20) << temp.vardas << std::setw(20) << temp.pavarde;
 
-    if (mediana)
-    {
-        std::sort(temp.pazymiai, temp.pazymiai + temp.pazymiuSk);
-        if (temp.pazymiuSk % 2 != 0)
-            temp.rezult = 0.4 * (double)temp.pazymiai[temp.pazymiuSk / 2] + 0.6 * temp.egzaminas;
-        else
-            temp.rezult = 0.4 * ((double)(temp.pazymiai[(temp.pazymiuSk - 1) / 2] + temp.pazymiai[temp.pazymiuSk / 2]) / 2.0) + 0.6 * temp.egzaminas;
-    }
-    else
-    {
-        for (int i = 0; i < temp.pazymiuSk; i++) temp.rezult += temp.pazymiai[i] * 1.0;
-        temp.rezult = (0.44 * (temp.rezult / temp.pazymiuSk)) + 0.6 * temp.egzaminas;
-    }
+    fout << std::setw(20) << temp.vardas << std::setw(20) << temp.pavarde;
 
-    cout << std::setw(20) << temp.rezult << endl;
-    delete[] temp.pazymiai;
+    std::sort(temp.pazymiai.begin(), temp.pazymiai.end());
+    if (temp.pazymiuSk % 2 != 0) temp.mediana = 0.4 * (double)temp.pazymiai[temp.pazymiuSk / 2] + 0.6 * temp.egzaminas;
+    else temp.mediana = 0.4 * ((double)(temp.pazymiai[(temp.pazymiuSk - 1) / 2] + temp.pazymiai[temp.pazymiuSk / 2]) / 2.0) + 0.6 * temp.egzaminas;
+
+    for (int x = 0; x < temp.pazymiuSk; x++) temp.vidurkis += temp.pazymiai[x] * 1.0;
+    temp.vidurkis = (0.4 * (temp.vidurkis / temp.pazymiuSk)) + 0.6 * temp.egzaminas;
+
+    fout << std::setw(20) << temp.mediana << std::setw(20) << temp.vidurkis << endl;
+
 }
 
 int IntTikrinimas()
@@ -286,7 +270,7 @@ void skaitymas(std::ifstream& fin, std::vector<string>& length, std::vector<moki
         for (auto& el : length)
         {
             fin >> p;
-            t.pazymiuSk.push_back(p);
+            t.pazymiai.push_back(p);
         }
         fin >> t.egzaminas;
         t.pazymiuSk = t.pazymiai.size(); // what it do?
