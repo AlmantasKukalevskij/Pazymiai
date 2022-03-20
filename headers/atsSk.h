82b5320f7da59385ed0e2460bfc4d65b3c8fbc79
AlmantasKukalevskij <92589309+AlmantasKukalevskij@users.noreply.github.com>
2022-03-20 17:58:05 +0200

    headeriai

---
+++ headers/atsSk.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "data.h"
+
+double vidurkis(vector<int> pazymys, int egzaminas);
+double mediana(vector<int> pazymys, int egzaminas);
+void atsSk(vector<mokinys>& mokiniai, bool rezultatas, bool manual);
+
+void mokiniuRusiavimas(std::stringstream& kietiakai, std::stringstream& vargsai, vector<mokinys>& mokiniai);
