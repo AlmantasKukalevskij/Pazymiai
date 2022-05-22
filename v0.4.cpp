a9d42d1a3ec092b066cf4d47eeb001c879f26ce1
AlmantasKukalevskij <92589309+AlmantasKukalevskij@users.noreply.github.com>
2022-05-22 20:17:43 +0300

    Add files via upload
    done

--- v0.4.cpp
+++ v0.4.cpp
@@ -2,47 +2,105 @@
 #include "tikrinimas.h"
 #include "IvestisIsvestis.h"
 #include "atsSk.h"
+#include "generavimas.h"
 
-bool egzaminas;
-bool pazymys;
-bool rezultatas;
-bool manual;
+#include <chrono>
+
+
+
+
 
 int main()
 {
+    auto programStart = hrClock::now();
+
     srand(time(NULL));
 
     bool kurimas = false;
-    bool mediana = false;
+    // bool mediana = false;
     bool mokiniuSkaicius = false;
-    string Mediana;
+    bool failoGeneravimas = false;
+    bool failoSkaitymas = false;
+
     string generuotiNd;
     string MokiniuSkaicius;
     string failoNuskaitymas;
-
+    string atsFailoGeneravimas;
     int mokiniuSk = 0;
 
-
+    string genFile_name;
 
     vector<mokinys> VisiMokiniai;
-    vector<string> length;
-    std::ofstream fout("atsakymai.txt");
+
+    //vector<string> length;
+    //std::ofstream fout("atsakymai.txt");
+
+    //cout << "Ar norite sugeneruoti faila?(taip/ne)" << endl;
+    //atsFailoGeneravimas = atsakymoTikrinimas();
+    //if (atsFailoGeneravimas=="taip")
+    //{
+    //    cin.ignore();
+    //    cout << "Iveskite norima failo pavadinima(kitaip liks mokiniai.txt)";
+    //    
+    //}
+
+
 
     cout << "Ar norite vesti pazymius ranka (taip), kitaip (ne) pazymiai bus nuskaityti is failo" << endl;
     failoNuskaitymas = atsakymoTikrinimas();
-
-
     if (failoNuskaitymas == "ne")
     {
-        try
+        cout << "Ar norite mokiniu duomenis sugeneruoti automatiskai? (taip/ne) "; atsFailoGeneravimas = atsakymoTikrinimas();
+        if (atsFailoGeneravimas == "taip")
         {
-            std::ifstream fin("mokiniai.txt");
-            skaitymas(fin, length, VisiMokiniai);
+
+            cin.ignore();
+            cout << "Iveskite failo pavadinima (kitaip bus mokiniai.txt)";
+            getline(cin, genFile_name);
+            genFile_name.empty() ? genFile_name = "mokiniai1.txt" : genFile_name += ".txt";
+            cout << "Kiek generuoti mokiniu?" << endl;
+            int mokSkaicius;
+            mokSkaicius = IntTikrinimas();
+            VisiMokiniai.reserve(mokSkaicius);
+            cout << "Kiek generuoti namu darbu?" << endl;
+            int ndSkaicius;
+            ndSkaicius = IntTikrinimas();
+            cout << "===================" << endl;
+            cout << mokSkaicius << "irasu testavimas" << endl;
+            auto genStart = hrClock::now();
+            failoGen(mokSkaicius, genFile_name, ndSkaicius);
+            cout << "Failo generavimo laikas: " << durationDouble(hrClock::now() - genStart).count() << " sek" << endl;
+
+            buferioSkaitymas(VisiMokiniai, genFile_name);
         }
-        catch (const std::exception&)
-        {
-            cout << "ivesties txt failas mokiniai nerastas" << endl;
+        else {
+            vector<string> Antraste;
+            try
+            {
+                buferioSkaitymas(VisiMokiniai, "mokiniai.txt");
+            }
+            catch (const std::exception&)
+            {
+                cout << "ivesties txt failas mokiniai nerastas" << endl;
+            }
+
         }
+
+        galutinisMedVid(VisiMokiniai);
+
+        std::sort(VisiMokiniai.begin(), VisiMokiniai.end(), [](mokinys& a, mokinys& b) {return a.vardas < b.vardas;  });
+
+        auto sortStart = hrClock::now();
+        vector<mokinys> kietekai;
+        vector<mokinys> vargsai;
+        MokiniuSortiravimas(kietekai, vargsai, VisiMokiniai);
+        cout << "Mokinius padalinti i dvi grupes uztruko: " << durationDouble(hrClock::now() - sortStart).count() << " sek." << endl;
+
+        auto newWrite = hrClock::now();
+        vectoriusToFile("kietekai.txt", kietekai);
+        vectoriusToFile("vargsai.txt", vargsai);
+        cout << "Surusiuotu mokini? isvedimas i naujus failus uztruko: " << durationDouble(hrClock::now() - newWrite).count() << " sek." << endl;
+
 
 
     }
@@ -91,26 +149,26 @@ int main()
             string mok;
             while (true)
             {
-                cout << "Ar norite irasyti dar viena mokini? (taip/ne) ";  mok = atsakymoTikrinimas();
-                if (mok == "ne")
-                    break;
                 mokinys data;
                 ivestis(data, kurimas);
                 VisiMokiniai.push_back(data);
+                cout << "Ar norite irasyti dar viena mokini? (taip/ne) ";  mok = atsakymoTikrinimas();
+                if (mok == "ne")
+                    break;
+
             }
 
         }
 
-
+        galutinisMedVid(VisiMokiniai);
+        std::sort(VisiMokiniai.begin(), VisiMokiniai.end(), [](mokinys& a, mokinys& b) {return a.vardas < b.vardas; });
+        buferioRasymas(VisiMokiniai);
     }
 
 
+    cout << "Programa veike: " << durationDouble(hrClock::now() - programStart).count() << " sek." << endl;
+    cout << "==============================" << endl;
 
-    std::sort(VisiMokiniai.begin(), VisiMokiniai.end(), [](mokinys& a, mokinys& b) {return a.vardas < b.vardas; });
-    fout << std::setw(20) << "VARDAS" << std::setw(20) << "PAVARDE" << std::setw(20) << "VIDURKIS" << std::setw(20) << "MEDIANA" << endl;
-    fout << "====================================================================================" << endl;
-
-    for (mokinys mokinys : VisiMokiniai) isved(mokinys, fout);
 
 
 }
